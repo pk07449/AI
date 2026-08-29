df16042db04ab806635b2c81633ff617d2d8a80b
Pankaj Kumar <java.j2ee.pankaj@gmail.com>
2026-08-29 23:26:39 +0530

    added first mcp server sample partial code commit

---
+++ mcp.server/src/main/java/com/pankaj/ai/mcp/server/first/Application.java
@@ -0,0 +1,13 @@
+package com.pankaj.ai.mcp.server.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ mcp.server/src/test/java/com/pankaj/ai/mcp/server/first/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankaj.ai.mcp.server.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
