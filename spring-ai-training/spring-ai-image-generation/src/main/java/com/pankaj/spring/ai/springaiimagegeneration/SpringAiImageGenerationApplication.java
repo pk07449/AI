9f3d7ad426c85b1dfb0ab02c12048e3ab9d5b213
Pankaj Kumar <java.j2ee.pankaj@gmail.com>
2026-09-21 19:03:32 +0530

    added required files

---
+++ spring-ai-training/spring-ai-image-generation/src/main/java/com/pankaj/spring/ai/springaiimagegeneration/SpringAiImageGenerationApplication.java
@@ -0,0 +1,14 @@
+package com.pankaj.spring.ai.springaiimagegeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiImageGenerationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiImageGenerationApplication.class, args);
+        //TODO: need to implement image api
+    }
+
+}
---
+++ spring-ai-training/spring-ai-image-generation/src/test/java/com/pankaj/spring/ai/springaiimagegeneration/SpringAiImageGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankaj.spring.ai.springaiimagegeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiImageGenerationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
